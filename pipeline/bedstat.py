356c70cbe75319e27f3410d41eb5eb100d239776
OD <oddodaoddo@gmail.com>
2019-09-21 17:28:47 -0400

    Add pypiper pipeline script to process bed file

--- pipeline/bedstat.py
+++ pipeline/bedstat.py
@@ -1,28 +1,48 @@
 #!/usr/bin/env python
 
 from argparse import ArgumentParser
-import pypiper, os
+import pypiper, os, sys
 
 parser = ArgumentParser(
     description="A pipeline to read a file in BED format and produce metadata in JSON format.")
 
-parser = pypiper.add_pypiper_args(parser, groups=["pypiper", "common", "looper"],
-                                    args=["output-parent", "input"],
-                                    required=['sample-name', 'output-parent', 'input'])
+parser.add_argument('--bedfile', help='full path to bed file to process')
+parser.add_argument('--outfolder', default='output', help='folder to put images and json files in')
+
+parser = pypiper.add_pypiper_args(parser, groups=["pypiper", "common", "looper"], required=['bedfile'])
 
 args = parser.parse_args()
 
-#outfolder = "bed_to_json_output/"
-outfolder = os.path.abspath(os.path.join(args.output_parent, args.sample_name))
+# get all the arguments and prepare to call R script
+if (args.bedfile is None):
+    parser.print_help()
+    sys.exit()
 
-pm = pypiper.PipelineManager(name="bed-to-json-pipeline", outfolder=outfolder, args=args)
+# get the --bedfile argument for the R script
+bfile = args.bedfile
 
-raw_folder = os.path.join(outfolder, "raw/")
+# get the --fileid argument for the R script
+split_path = os.path.split(bfile)
 
-local_input_file = raw_folder + "/" + args.input
+bedfile_portion = split_path[1]
+dot_separator_idx = bedfile_portion.find('.')
+if (dot_separator_idx < 0):
+    fileid = bedfile_portion
+else:
+    fileid = bedfile_portion[0:dot_separator_idx]
 
-target = os.path.join(outfolder, "bedfile.out")
-command = "Rscript regionstat.R"
+# get the output folder argument for the R script
+outfolder = os.path.abspath(os.path.join(args.outfolder, fileid))
+
+# try to create the directory and ignore failure if it already exists
+os.makedirs(outfolder, exist_ok=True)
+
+pm = pypiper.PipelineManager(name="bedstat-pipeline", outfolder=outfolder, args=args)
+
+command = "Rscript tools/regionstat.R --bedfile=%s --fileid=%s --outputfolder=%s" % (bfile, fileid, outfolder)
+
+target = os.path.abspath(os.path.join(outfolder, bedfile_portion))
 
 pm.run(command, target)
+
 pm.stop_pipeline()
