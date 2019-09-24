e2571530d721652858895f68d00f214f3ac605d1
OD <oddodaoddo@gmail.com>
2019-09-24 09:25:54 -0400

    Running version of lola processing script to produce looper sample csv file. Initial looper pipeline config files.

--- scripts/process_LOLA.py
+++ scripts/process_LOLA.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# purpose of this script is to run through LOLA Core DB and produce a CSV file as input for looper
+# Author: Ognen Duzlevski
+
 from argparse import ArgumentParser
 import os, sys
 from pathlib import Path
@@ -7,21 +10,51 @@ from pathlib import Path
 # for reading TSV files
 import csv
 
+class LOLAIndexFileError(Exception):
+    pass
+
 # process index file from LOLA core DB
 # expects a base directory for the collection
 # and base_dir/regions to contain the bed files
 # the index file is expected to be in tsv format
 # however, the header structure is not fixed
+# if the file happens to be in csv format, we try to make it right
 def process_index_file(base_dir, idx_file):
+    #print("%s" % idx_file)
+    bed_path_base = os.path.abspath(os.path.join(base_dir, "regions"))
     with open(idx_file) as tsvfile:
-        reader = csv.DictReader(tsvfile, dialect='excel-tab')
-        for row in reader:
-            # do something with each row
-            pass
+        try:
+            reader = csv.DictReader(tsvfile, dialect='excel-tab')
+            n = reader.__next__()
+            # handle the case where index.txt is csv, not tsv
+            l = list(n)
+            if len(n) == 1:
+                # csv case
+                if (l[0].find(',') != -1):
+                    tsvfile.seek(0)
+                    reader = csv.DictReader(tsvfile)
+                    n = reader.__next__()
+                else:
+                    raise LOLAIndexFileError
+            for row in reader:
+                s = ''
+                if 'filename' in row:
+                    s = os.path.abspath(os.path.join(bed_path_base, row['filename']))
+                else:
+                    raise LOLAIndexFileError
+                if 'cellType' in row:
+                    s = s + "," + row['cellType']
+                else:
+                    s = s + ',' + "unknown"
+                print(s)
+        except Exception as e:
+            print("Exception reading %s file" % idx_file)
+            raise
 
 def process_collection_file(base_dir, cfile):
-    print("Processing collection file %s" % cfile)
-    print("Base dir: %s" % base_dir)
+    #print("Processing collection file %s" % cfile)
+    #print("Base dir: %s" % base_dir)
+    pass
 
 if __name__ == '__main__':
     parser = ArgumentParser(description="Script to process LOLA database into PEP format.")
@@ -43,6 +76,8 @@ if __name__ == '__main__':
     # each file has a header and is \t delimited, however, header structure is not fixed
     full_path = os.path.abspath(os.path.join(lola_base, genome))
 
+    # what we are interested in for now from index.txt files
+    print("input,celltype")
     # search for subfolders and index.txt
     for filename in Path(full_path).glob('**/index.txt'):
         # break down the path from each index.txt file
@@ -51,14 +86,14 @@ if __name__ == '__main__':
         try:
             path_parts = os.path.split(filename)
             if (len(path_parts) != 2): break
-            collection_file = os.path.abspath(os.path.join(path_parts[0], 'collection.txt'))
-            cpath = Path(collection_file)
-            if (cpath.exists() and cpath.is_file()):
-                # we also have a collection file
-                process_index_file(path_parts[0], filename)
-                process_collection_file(path_parts[0], cpath)
+            # process index file
+            process_index_file(path_parts[0], filename)
+            
+            # process collections file
+            #collection_file = os.path.abspath(os.path.join(path_parts[0], 'collection.txt'))
+            #cpath = Path(collection_file)
+            #if (cpath.exists() and cpath.is_file()):
+                # we also have a collection file      
+            #    process_collection_file(path_parts[0], cpath)
         except Exception as e:
             print("Error accessing file: {0}".format(e))
-
-
-        
