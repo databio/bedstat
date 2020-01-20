90a6ccfd991de54b2e13a671e68816c3fa083535
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-01-20 08:35:28 -0500

    file gzipping, insert data into elastic, organize consts

--- pipeline/bedstat.py
+++ pipeline/bedstat.py
@@ -1,15 +1,34 @@
 #!/usr/bin/env python
 from argparse import ArgumentParser
-import pypiper, os, sys
+import pypiper
 from elasticsearch import Elasticsearch
-from elasticsearch import helpers
-from elasticsearch.serializer import JSONSerializer
 import json
+import gzip
+import shutil
+import yaml
+import os
+import sys
+
+# consts (some are shared with bedhost, common source needed)
+DEFAULT_BED_INDEX = "bedstat_bedfiles"
+SEARCH_TERMS = ['cellType', 'cellTypeSubtype', 'antibody', 'mappingGenome', 'description',
+                'tissue', 'species', 'protocol', 'genome']
+
 
 parser = ArgumentParser(
     description="A pipeline to read a file in BED format and produce metadata in JSON format.")
 
 parser.add_argument('--bedfile', help='full path to bed file to process', required=True)
+parser.add_argument('--nodbcommit', help='do not try to commit json outout to back end database', action='store_true')
+parser.add_argument('--dbhost',
+                    help='use database host address/name to connect to',
+                    required='--nodbcommit' not in sys.argv,
+                    default='localhost')
+# parser argument for yaml - to pass on more sample metadata into the database
+parser.add_argument("-y", "--sample-yaml",
+                    dest="sample_config",
+                    help="Yaml config file with sample attributes.",
+                    type=str)
 # parser.add_argument('--outfolder', default=output_dir, help='folder to put images and json files in')
 
 parser = pypiper.add_pypiper_args(parser, args=["genome"], groups=["pypiper", "common", "looper", "ngs"],
@@ -28,8 +47,8 @@ fileid = os.path.splitext(bedfile_portion)[0]
 # get the output folder argument for the R script
 outfolder = os.path.abspath(os.path.join(outfolder, fileid))
 
-# try to create the directory and ignore failure if it already exists
-# os.makedirs(outfolder, exist_ok=True)
+# get the sample line from the yaml config file
+y = yaml.safe_load(open(args.sample_config, "r"))
 
 pm = pypiper.PipelineManager(name="bedstat-pipeline", outfolder=outfolder, args=args)
 
@@ -37,6 +56,19 @@ rscript_path = os.path.join(os.path.pardir(os.path.dirname(os.path.abspath(__fil
 
 cmd_vars = dict(rscript=rscript_path, bed=args.bedfile, id=fileid, out=outfolder, genome=args.genome_assembly)
 command = "Rscript {rscript} --bedfile={bed} --fileid={id} --outputfolder={out} --genome={genome}".format(cmd_vars)
+
+# gzip the original bed file and keep it with the pipeline results
+try:
+    dst_path = os.path.abspath(os.path.join(outfolder, bedfile_portion))
+    with open(args.bedfile, 'rb') as f_in:
+        with gzip.open(dst_path + '.gz', 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+    # create a symlink to make the .gz easier to find
+    link_dest = os.path.abspath(os.path.join(outfolder, "raw_bedfile"))
+    if not os.path.exists(link_dest):
+        os.symlink(bedfile_portion + ".gz", link_dest)
+except Exception as e:
+    raise e
 
 target = os.path.abspath(os.path.join(outfolder, bedfile_portion))
 
@@ -52,7 +84,16 @@ if os.path.splitext(bedfile_portion)[1] != '':
         json_file_path = os.path.abspath(os.path.join(outfolder, json_file))
         with open(json_file_path, 'r', encoding='utf-8') as f:
             data = json.loads(f.read())
-        es.index(index="bedstat_bedfiles", doc_type='doc', body=data)
+            for key in SEARCH_TERMS:
+                try:
+                    data[key] = y[key]
+                except KeyError:
+                    pm.warning("Can't find key: {}".format(key))
+                    pm.warning(y)
+                # enrich the data from R with the data from the sample line itself
+            pm.info("Inserting into database...")
+            es.index(index=DEFAULT_BED_INDEX, body=data)
+        es.index(index=DEFAULT_BED_INDEX, doc_type='doc', body=data)
     except Exception as e:
         print(e)
 
