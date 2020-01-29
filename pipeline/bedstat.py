cd42c012a1742a8be7e3b72e44b529814ad615fa
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-01-29 09:39:51 -0500

    update pipeline to use bbconf

--- pipeline/bedstat.py
+++ pipeline/bedstat.py
@@ -1,83 +1,67 @@
 #!/usr/bin/env python3
+"""
+bedfile statistics generating pipeline
+"""
+
+__author__ = ["Michal Stolarczyk", "Ognen Duzlevski"]
+__email__ = "michal@virginia.edu"
+__version__ = "0.0.1"
+
 from argparse import ArgumentParser
-from elasticsearch import Elasticsearch
 import json
-import gzip
-import shutil
 import yaml
 import os
 
 import pypiper
 from bbconf.const import *
+import bbconf
 
 parser = ArgumentParser(description="A pipeline to read a file in BED format and produce metadata in JSON format.")
 
 parser.add_argument('--bedfile', help='a full path to bed file to process', required=True)
+parser.add_argument("--bedbase-config", dest="bedbase_config", type=str, required=False, default=None,
+                    help="a path to the bedbase configuratiion file")
 parser.add_argument('--nodbcommit', help='whether the json commit to the database should be skipped',
                     action='store_true')
-parser.add_argument('--dbhost', help='a database host address/name to connect to', required=False, default='localhost')
-# parser argument for yaml - to pass on more sample metadata into the database
-parser.add_argument("-y", "--sample-yaml", dest="sample_config", type=str, required=False,
+parser.add_argument("-y", "--sample-yaml", dest="sample_yaml", type=str, required=False,
                     help="a yaml config file with sample attributes to pass on more metadata into the database")
-
-parser = pypiper.add_pypiper_args(parser, args=["genome"], groups=["pypiper", "common", "looper", "ngs"],
-                                  required=['bedfile', 'genome'])
+parser = pypiper.add_pypiper_args(parser, groups=["pypiper", "common", "looper", "ngs"])
 
 args = parser.parse_args()
 
-# get the --fileid argument for the R script
+bbc = bbconf.BedBaseConf(filepath=bbconf.get_bedbase_cfg(args.bedbase_config))
+
 bedfile_name = os.path.split(args.bedfile)[1]
 fileid = os.path.splitext(bedfile_name)[0]
-
-# get the output folder argument for the R script
-outfolder = os.path.abspath(os.path.join(args.output_parent, fileid))
-
-# gzip the original bed file and keep it with the pipeline results
-try:
-    dst_path = os.path.abspath(os.path.join(outfolder, bedfile_name))
-    with open(args.bedfile, 'rb') as f_in:
-        with gzip.open(dst_path + '.gz', 'wb') as f_out:
-            shutil.copyfileobj(f_in, f_out)
-    # create a symlink to make the .gz easier to find
-    link_dest = os.path.abspath(os.path.join(outfolder, "raw_bedfile"))
-    if not os.path.exists(link_dest):
-        os.symlink(bedfile_name + ".gz", link_dest)
-except Exception as e:
-    raise e
+outfolder = os.path.abspath(os.path.join(bbc.path.bedstat_output, fileid))
 
 pm = pypiper.PipelineManager(name="bedstat-pipeline", outfolder=outfolder, args=args)
 rscript_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "tools", "regionstat.R")
 assert os.path.exists(rscript_path), FileNotFoundError("'{}' script not found".format(rscript_path))
 cmd_vars = dict(rscript=rscript_path, bed=args.bedfile, id=fileid, out=outfolder, genome=args.genome_assembly)
 command = "Rscript {rscript} --bedfile={bed} --fileid={id} --outputfolder={out} --genome={genome}".format(**cmd_vars)
-target = os.path.abspath(os.path.join(outfolder, bedfile_name))
+json_file_path = os.path.abspath(os.path.join(outfolder, fileid + ".json"))
 
-pm.run(command, target)
+pm.run(cmd=command, target=json_file_path)
 
 # now get the resulting json file and load it into elasticsearch
 # it the file exists, of course
-if not args.nodbcommit and os.path.splitext(bedfile_name)[1] != '':
+if not args.nodbcommit:
     # open connection to elastic
-    try:
-        es = Elasticsearch([{'host': args.dbhost}])
-        json_file = os.path.splitext(bedfile_name)[0] + ".json"
-        json_file_path = os.path.abspath(os.path.join(outfolder, json_file))
-        with open(json_file_path, 'r', encoding='utf-8') as f:
-            data = json.loads(f.read())
-            # get the sample line from the yaml config file
-            if args.sample_config:
-                y = yaml.safe_load(open(args.sample_config, "r"))
-                for key in SEARCH_TERMS:
-                    try:
-                        data[key] = y[key]
-                    except KeyError:
-                        pm.warning("Can't find key: {}".format(key))
-                        pm.warning(y)
-                # enrich the data from R with the data from the sample line itself
-            pm.info("Inserting metadata into database: {}".format(args.dbhost))
-            es.index(index=BED_INDEX, body=data)
-        es.index(index=BED_INDEX, doc_type='doc', body=data)
-    except Exception as e:
-        print(e)
+    bbc.establish_elasticsearch_connection()
+    with open(json_file_path, 'r', encoding='utf-8') as f:
+        data = json.loads(f.read())
+    if args.sample_yaml:
+        # get the sample line from the yaml config file
+        y = yaml.safe_load(open(args.sample_yaml, "r"))
+        # enrich the data from R with the data from the sample line itself
+        for key in SEARCH_TERMS:
+            try:
+                data[key] = y[key]
+            except KeyError:
+                pm.warning("Can't find key: {}".format(key))
+    pm.debug("Data: {}".format(data))
+    data[BEDFILE_PATH_KEY] = bedfile_name + ".gz"
+    bbc.insert_bedfile_data(data=data)
 
 pm.stop_pipeline()
