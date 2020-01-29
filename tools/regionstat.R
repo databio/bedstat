720614e32f2787376d4ee7626623e890bc8441db
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-01-29 09:06:03 -0500

    change plot sizes

--- tools/regionstat.R
+++ tools/regionstat.R
@@ -2,11 +2,6 @@ library(GenomicDistributions)
 library(optparse)
 library(tools)
 
-## we need to parameterize LOLACore location
-## and file name for bed file
-
-defaultLOLADB = "/ext/qumulo/resources/regions/LOLACore/hg38"
-
 option_list = list(
     make_option(c("--bedfile"), type="character", default=NULL, 
               help="bed file to process", metavar="character"),
@@ -30,20 +25,6 @@ if (is.null(opt$fileid)) {
     stop("fileID input missing.")
 }
 
-##rdb = LOLA::loadRegionDB("/home/maketo/dev/sheffield/ext/qumulo/LOLAweb/databases/LOLACore/hg38")
-
-##rdb$regionGRL
-##rdb$regionAnno[1,"filename"]
-
-##length(rdb$regionGRL)
-##lapply(seq(1, 3), 
-##	function(x) {
-##		doitall(rdb$regionGRL[[x]], rdb$regionAnno[x,]$filename)
-##	}
-##	)
-
-##fileid <- "E125-DNase.macs2"
-##fn <- "/home/maketo/dev/sheffield/ext/qumulo/LOLAweb/databases/LOLACore/hg38/cistrome_cistrome/regions/Human_HepG2-liver-cells_FoxA2_No-treatment_Wadelius.bed"
 
 doitall <- function(query, fname, fileid, genome) {
 
@@ -55,39 +36,37 @@ doitall <- function(query, fname, fileid, genome) {
 	g = plotFeatureDist(TSSdist, featureName="TSS")
     # different width/heights for presenting on screen in HTML and for PDFs
     ggplot2::ggsave(paste0(outfolder, "/", fileid, "_tssdist.png"), g, device="png", width=8, height=8, units="cm")
-    ggplot2::ggsave(paste0(outfolder, "/", fileid, "_tssdist.pdf"), g, device="pdf", width=8, height=8, units="cm")
+    ggplot2::ggsave(paste0(outfolder, "/", fileid, "_tssdist.pdf"), g, device="pdf", width=12, height=12, units="cm")
     
 	x = calcChromBinsRef(query, genome)
 	g = plotChromBins(x)
     # different width/heights for presenting on screen in HTML and for PDFs    
-    ggplot2::ggsave(paste0(outfolder, "/", fileid, "_chrombins.png"), g, device="png", width=18, height=12, units="cm")
+    ggplot2::ggsave(paste0(outfolder, "/", fileid, "_chrombins.png"), g, device="png", width=12, height=8, units="cm")
     ggplot2::ggsave(paste0(outfolder, "/", fileid, "_chrombins.pdf"), g, device="pdf", width=18, height=12, units="cm")
     
 	gcvec = calcGCContentRef(query, genome)
 	g = plotGCContent(gcvec)
     # different width/heights for presenting on screen in HTML and for PDFs
-	ggplot2::ggsave(paste0(outfolder, "/", fileid, "_gccontent.png"), g, device="png", width=6, height=6, units="cm")
-	ggplot2::ggsave(paste0(outfolder, "/", fileid, "_gccontent.pdf"), g, device="pdf", width=6, height=6, units="cm")
+	ggplot2::ggsave(paste0(outfolder, "/", fileid, "_gccontent.png"), g, device="png", width=8, height=8, units="cm")
+	ggplot2::ggsave(paste0(outfolder, "/", fileid, "_gccontent.pdf"), g, device="pdf", width=12, height=12, units="cm")
     
 	gp = calcPartitionsRef(query, genome)
 	gp$Perc = gp$Freq/length(query)
 	g = plotPartitions(gp)
     # different width/heights for presenting on screen in HTML and for PDFs    
-	ggplot2::ggsave(paste0(outfolder, "/", fileid, "_partitions.png"), g, device="png", width=9, height=9, units="cm")
-	ggplot2::ggsave(paste0(outfolder, "/", fileid, "_partitions.pdf"), g, device="pdf", width=9, height=9, units="cm")
+	ggplot2::ggsave(paste0(outfolder, "/", fileid, "_partitions.png"), g, device="png", width=8, height=8, units="cm")
+	ggplot2::ggsave(paste0(outfolder, "/", fileid, "_partitions.pdf"), g, device="pdf", width=12, height=12, units="cm")
     
-	##l = list()
-	bedmeta = list(id=fileid,
+	bedmeta = list(
+	  id=fileid,
 		gc_content=mean(gcvec),
 		num_regions=length(query),
 		mean_abs_tss_dist=mean(abs(TSSdist), na.rm=TRUE),
 		genomic_partitions=gp,
-        md5sum=md5s)
-	##l[[fileid]]=bedmeta
-	##l
+		md5sum=md5s
+	)
 
 	write(jsonlite::toJSON(bedmeta, pretty=TRUE), paste0(outfolder,"/",fileid,".json"))
-
 }
 
 # set query to bed file
@@ -96,6 +75,5 @@ fn = opt$bedfile
 outfolder = opt$outputfolder
 genome = opt$genome
 
-#' query = rtracklayer::import(fn)
 query = LOLA::readBed(fn)
-doitall(query,fn,fileid,genome)
+doitall(query, fn, fileid, genome)
