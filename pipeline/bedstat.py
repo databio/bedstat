2d0a3774e880082f1a4f56eaf5f48c6c7497b1fa
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-02-05 13:47:18 -0500

    add possibility of decoupling database commit from stats calculation

--- pipeline/bedstat.py
+++ pipeline/bedstat.py
@@ -21,10 +21,13 @@ parser = ArgumentParser(description="A pipeline to read a file in BED format and
 parser.add_argument('--bedfile', help='a full path to bed file to process', required=True)
 parser.add_argument("--bedbase-config", dest="bedbase_config", type=str, required=False, default=None,
                     help="a path to the bedbase configuratiion file")
-parser.add_argument('--nodbcommit', help='whether the json commit to the database should be skipped',
-                    action='store_true')
 parser.add_argument("-y", "--sample-yaml", dest="sample_yaml", type=str, required=False,
                     help="a yaml config file with sample attributes to pass on more metadata into the database")
+exclusive_group = parser.add_mutually_exclusive_group()
+exclusive_group.add_argument('--no-db-commit', action='store_true',
+                             help='whether the JSON commit to the database should be skipped')
+exclusive_group.add_argument('--just-db-commit', action='store_true',
+                             help='whether just to commit the JSON to the database')
 parser = pypiper.add_pypiper_args(parser, groups=["pypiper", "common", "looper", "ngs"])
 
 args = parser.parse_args()
@@ -34,19 +37,20 @@ bbc = bbconf.BedBaseConf(filepath=bbconf.get_bedbase_cfg(args.bedbase_config))
 bedfile_name = os.path.split(args.bedfile)[1]
 fileid = os.path.splitext(os.path.splitext(bedfile_name)[0])[0]  # twice since there are 2 exts
 outfolder = os.path.abspath(os.path.join(bbc.path.bedstat_output, fileid))
-
-pm = pypiper.PipelineManager(name="bedstat-pipeline", outfolder=outfolder, args=args)
-rscript_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "tools", "regionstat.R")
-assert os.path.exists(rscript_path), FileNotFoundError("'{}' script not found".format(rscript_path))
-cmd_vars = dict(rscript=rscript_path, bed=args.bedfile, id=fileid, out=outfolder, genome=args.genome_assembly)
-command = "Rscript {rscript} --bedfile={bed} --fileid={id} --outputfolder={out} --genome={genome}".format(**cmd_vars)
 json_file_path = os.path.abspath(os.path.join(outfolder, fileid + ".json"))
 
-pm.run(cmd=command, target=json_file_path)
+if not args.just_db_commit:
+    pm = pypiper.PipelineManager(name="bedstat-pipeline", outfolder=outfolder, args=args)
+    rscript_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "tools", "regionstat.R")
+    assert os.path.exists(rscript_path), FileNotFoundError("'{}' script not found".format(rscript_path))
+    cmd_vars = dict(rscript=rscript_path, bed=args.bedfile, id=fileid, out=outfolder, genome=args.genome_assembly)
+    command = "Rscript {rscript} --bedfile={bed} --fileid={id} --outputfolder={out} --genome={genome}".format(**cmd_vars)
+    pm.run(cmd=command, target=json_file_path)
+    pm.stop_pipeline()
 
-# now get the resulting json file and load it into elasticsearch
+# now get the resulting json file and load it into Elasticsearch
 # it the file exists, of course
-if not args.nodbcommit:
+if not args.no_db_commit:
     # open connection to elastic
     bbc.establish_elasticsearch_connection()
     with open(json_file_path, 'r', encoding='utf-8') as f:
@@ -59,9 +63,7 @@ if not args.nodbcommit:
             try:
                 data[key] = y[key]
             except KeyError:
-                pm.warning("Can't find key: {}".format(key))
+                print("Can't find key: {}".format(key))
     data[BEDFILE_PATH_KEY] = [args.bedfile]  # as a list to match how the statistics in regionstat are saved
-    pm.info("Data: {}".format(data))
+    print("Data: {}".format(data))
     bbc.insert_bedfiles_data(data=data)
-
-pm.stop_pipeline()
