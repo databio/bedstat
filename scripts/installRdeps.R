44bb90de09451ed6726ef56d22e5fc0dfba115d2
joseverdezoto <j.esiempreliga@hotmail.com>
2020-05-28 11:25:44 -0400

    add GenomicDistributionsData to R deps

--- scripts/installRdeps.R
+++ scripts/installRdeps.R
@@ -12,5 +12,8 @@
 .install_pkg("optparse")
 .install_pkg("devtools")
 devtools::install_github("databio/GenomicDistributions")
+.install_pkg("http://big.databio.org/GenomicDistributionsData/GenomicDistributionsData_0.0.1.tar.gz", repos=NULL)
 .install_pkg("GenomicRanges", bioc=TRUE)
+.install_pkg("GenomicFeatures", bioc=TRUE)
+.install_pkg("ensembldb", bioc=TRUE)
 .install_pkg("LOLA", bioc=TRUE)
