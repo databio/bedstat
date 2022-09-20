1062812be29cc5e63f81cbb063fe8d6464fd8add
Khoroshevskyi <sasha99250@gmail.com>
2022-09-20 11:40:19 -0400

    Dependencies R 2

--- scripts/installRdeps.R
+++ scripts/installRdeps.R
@@ -11,15 +11,16 @@
 .install_pkg("R.utils")
 .install_pkg("BiocManager")
 .install_pkg("optparse")
-# .install_pkg("devtools")
+.install_pkg("devtools")
 .install_pkg("GenomicRanges", bioc=TRUE)
 .install_pkg("GenomicFeatures", bioc=TRUE)
 .install_pkg("ensembldb", bioc=TRUE)
 .install_pkg("LOLA", bioc=TRUE)
 .install_pkg("BSgenome", bioc=TRUE)
-if(!require(package = "GenomicDistributions", character.only=TRUE)) {
-    devtools::install_github("databio/GenomicDistributions")
-}
+.install_pkg("GenomicDistributions", bioc=TRUE)
+# if(!require(package = "GenomicDistributions", character.only=TRUE)) {
+#     devtools::install_github("databio/GenomicDistributions")
+# }
 if(!require(package = "GenomicDistributionsData", character.only=TRUE)) {
     install.packages("http://big.databio.org/GenomicDistributionsData/GenomicDistributionsData_0.0.2.tar.gz", repos=NULL)
 }
