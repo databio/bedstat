e4a5583203235d7b53432d34f829c5fb96d4cf2f
Khoroshevskyi <sasha99250@gmail.com>
2022-10-06 11:05:44 -0400

    fixed config db

--- tests/test_bedstat.py
+++ tests/test_bedstat.py
@@ -2,8 +2,10 @@ import bedstat
 import os
 import pytest
 
+
 file_path = "./tests"
 # file_path = "./"
+CONFIG_FILE = f"{file_path}/config_db_github.yaml"
 
 
 class TestBedstat:
@@ -12,7 +14,7 @@ class TestBedstat:
             bedfile=f"{file_path}/data/f1/AML_db358.bed.gz",
             genome_assembly="hg19",
             sample_yaml=f"{file_path}/data/f1/AML_db358_sample.yaml",
-            bedbase_config=f"{file_path}/config_db_local.yaml",
+            bedbase_config=CONFIG_FILE,
             bigbed=f"{file_path}/data/f1/",
             just_db_commit=True,
         )
