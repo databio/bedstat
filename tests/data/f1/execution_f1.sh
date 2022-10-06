6672b03740351b0ece7f1d117fb1b3d14e9e37be
Khoroshevskyi <sasha99250@gmail.com>
2022-10-06 11:27:58 -0400

    added db check

--- tests/data/f1/execution_f1.sh
+++
@@ -1,7 +0,0 @@
-
---bedfile ./AML_db358.bed.gz \
---genome hg19 \
---sample-yaml ./AML_db358_sample.yaml \
---bedbase-config ../bedstat_config.yaml \
---bigbed . \
---just-db-commit
--- tests/test_bedstat.py
+++ tests/test_bedstat.py
@@ -1,20 +1,26 @@
 import bedstat
 import os
 import pytest
+import bbconf
 
 
 file_path = "./tests"
 # file_path = "./"
 CONFIG_FILE = f"{file_path}/config_db_github.yaml"
+# CONFIG_FILE = f"{file_path}/config_db_local.yaml"
 
 
 class TestBedstat:
     def test_uploading(self):
         bedstat.run_bedstat(
             bedfile=f"{file_path}/data/f1/AML_db358.bed.gz",
-            genome_assembly="hg19",
             sample_yaml=f"{file_path}/data/f1/AML_db358_sample.yaml",
             bedbase_config=CONFIG_FILE,
             bigbed=f"{file_path}/data/f1/",
             just_db_commit=True,
+            genome_assembly="hg19",
         )
+
+    def test_db_record(self):
+        bbc = bbconf.BedBaseConf(config_path=CONFIG_FILE, database_only=True)
+        assert bbc.bed.record_count == 1
